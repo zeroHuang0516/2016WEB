059d79a659b558e9edfb0a2487374187849cb828
zeroHuang0516 <fantastic0516@gmail.com>
2016-11-02 07:35:48 +0800

    hw4

--- b03901156_hw4/dist/index.js
+++ b03901156_hw4/dist/index.js
@@ -1,6 +1,8 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -11,221 +13,214 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var _React = React,
     Component = _React.Component;
 
-var TodoApp = function (_React$Component) {
-  _inherits(TodoApp, _React$Component);
+var TodoApp = function (_Component) {
+	_inherits(TodoApp, _Component);
 
-  function TodoApp(props) {
-    _classCallCheck(this, TodoApp);
+	function TodoApp(props, context) {
+		_classCallCheck(this, TodoApp);
 
-    var _this = _possibleConstructorReturn(this, (TodoApp.__proto__ || Object.getPrototypeOf(TodoApp)).call(this, props));
+		var _this = _possibleConstructorReturn(this, (TodoApp.__proto__ || Object.getPrototypeOf(TodoApp)).call(this, props, context));
 
-    _this.state = { count: 0, todoList: [] };
+		_this.state = {
+			todos: [{
+				value: '',
+				completed: false
+			}],
+			text: ''
+		};
+		_this.updateValue = _this.updateValue.bind(_this);
+		_this.handleEnter = _this.handleEnter.bind(_this);
+		_this.Destroy = _this.Destroy.bind(_this);
+		_this.handleClick = _this.handleClick.bind(_this);
+		_this.DisplayLeft = _this.DisplayLeft.bind(_this);
+		_this.DisplaytodoItem = _this.DisplaytodoItem.bind(_this);
+		return _this;
+	}
 
-    // bind
-    _this.createNewList = _this.createNewList.bind(_this);
-    return _this;
-  }
+	_createClass(TodoApp, [{
+		key: 'updateValue',
+		value: function updateValue(e) {
+			this.setState({
+				text: e.target.value
+			});
+		}
+	}, {
+		key: 'handleEnter',
+		value: function handleEnter(ele) {
+			if (ele.keyCode === 13 && this.state.text.trim() !== '') {
+				this.setState({
+					todos: [{ value: this.state.text,
+						completed: false }].concat(_toConsumableArray(todos)),
+					text: ''
+				});
+				console.log("enter on");
+			};
+		}
+	}, {
+		key: 'Destroy',
+		value: function Destroy(idx) {
+			this.setState({
+				todos: this.state.todos.splice(idx, 1)
+			});
+		}
+	}, {
+		key: 'handleClick',
+		value: function handleClick(idx) {
+			var tmptodos = [].concat(_toConsumableArray(this.state.todos));
+			tmptodos[idx].completed = !tmptodos[idx].completed;
+			this.setState({ todos: tmptodos });
+		}
+	}, {
+		key: 'DisplaytodoItem',
+		value: function DisplaytodoItem(lists, idx) {
+			var _this2 = this;
 
-  _createClass(TodoApp, [{
-    key: "createNewList",
-    value: function createNewList(listItem) {
-      var newList = this.state.todoList.slice();
-      newList.push(listItem);
-      this.setState({
-        count: this.state.count + 1,
-        todoList: newList
-      });
-    } // another way: this.setState({count: this.state.count+1;
-    // todoList: this.state.todoList.concat([{text: listItem}])})
+			return React.createElement(TodoItem, {
+				onCheckClick: function onCheckClick() {
+					return _this2.handleClick(idx);
+				},
+				onCancel: function onCancel() {
+					return _this2.Destroy(idx);
+				},
+				content: lists["value"],
+				completed: lists["completed"],
+				key: idx
+			});
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			var _this3 = this;
 
-  }, {
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "section",
-        { className: "todoapp" },
-        React.createElement(HEADER, { createNewList: this.createNewList }),
-        React.createElement(MAIN, { count: this.state.count, todoList: this.state.todoList }),
-        React.createElement(FOOTER, { count: this.state.count })
-      );
-    }
-  }]);
+			var _state = this.state,
+			    todos = _state.todos,
+			    text = _state.text;
 
-  return TodoApp;
+			return React.createElement(
+				'section',
+				{ className: 'todoapp' },
+				React.createElement(
+					'header',
+					{ className: 'header' },
+					React.createElement(
+						'h1',
+						null,
+						'todos'
+					),
+					React.createElement('input', { className: 'new-todo',
+						value: text,
+						onChange: this.updateValue,
+						onKeyDown: this.handleEnter,
+						placeholder: 'What needs to be done?',
+						autofocus: true })
+				),
+				React.createElement(
+					'ul',
+					{ 'class': 'todo-list' },
+					todos.map(function (list, idx) {
+						return _this3.DisplaytodoItem(list, idx);
+					})
+				),
+				React.createElement(CountDisplay, null)
+			);
+		}
+	}]);
+
+	return TodoApp;
+}(Component);
+
+var TodoItem = function (_React$Component) {
+	_inherits(TodoItem, _React$Component);
+
+	function TodoItem() {
+		_classCallCheck(this, TodoItem);
+
+		return _possibleConstructorReturn(this, (TodoItem.__proto__ || Object.getPrototypeOf(TodoItem)).apply(this, arguments));
+	}
+
+	_createClass(TodoItem, [{
+		key: 'render',
+		value: function render() {
+			var _this5 = this;
+
+			var completed = this.props.completed;
+
+			return React.createElement(
+				'li',
+				{ className: completed ? "done" : "" },
+				React.createElement('input', { className: 'toggle',
+					type: 'checkbox',
+					checked: completed,
+					onChange: function onChange(e) {
+						return _this5.props.handleClick();
+					} }),
+				React.createElement(
+					'label',
+					null,
+					this.props.content
+				),
+				React.createElement('button', { className: 'delete',
+					onChange: function onChange(e) {
+						return _this5.props.Destroy();
+					} })
+			);
+		}
+	}]);
+
+	return TodoItem;
 }(React.Component);
 
-HEADER.propTypes = {
-  createNewList: React.PropTypes.func.isRequired
-};
+var CountDisplay = function (_React$Component2) {
+	_inherits(CountDisplay, _React$Component2);
 
-var HEADER = function (_Component) {
-  _inherits(HEADER, _Component);
+	function CountDisplay() {
+		_classCallCheck(this, CountDisplay);
 
-  function HEADER(props) {
-    _classCallCheck(this, HEADER);
+		return _possibleConstructorReturn(this, (CountDisplay.__proto__ || Object.getPrototypeOf(CountDisplay)).apply(this, arguments));
+	}
 
-    var _this2 = _possibleConstructorReturn(this, (HEADER.__proto__ || Object.getPrototypeOf(HEADER)).call(this, props));
+	_createClass(CountDisplay, [{
+		key: 'DisplayLeft',
+		value: function DisplayLeft() {
+			var count;
+			for (var i = 0; i < this.props.todos.length; i += 1) {
+				if (this.props.todos[i].completed) {
+					count += 1;
+				}
+			}
+			if (count !== 1) {
+				return React.createElement(
+					'span',
+					null,
+					count,
+					' items left'
+				);
+			} else if (count === 1) {
+				return React.createElement(
+					'span',
+					null,
+					'1 item left'
+				);
+			} else {
+				return React.createElement(
+					'span',
+					null,
+					'NO items'
+				);
+			}
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'footer',
+				{ className: 'todo-count' },
+				this.DisplayLeft()
+			);
+		}
+	}]);
 
-    _this2.handleEnter = _this2.handleEnter.bind(_this2);
-    return _this2;
-  }
-
-  _createClass(HEADER, [{
-    key: "handleEnter",
-    value: function handleEnter(ele) {
-      if (eve.keyCode === 13 && ele.target.value !== '') {
-        this.props.createNewList(ele.target.value);
-      }
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "header",
-        { className: "header" },
-        React.createElement(
-          "h1",
-          null,
-          "todos"
-        ),
-        React.createElement("input", {
-          className: "new-todo",
-          onKeyDown: this.handleEnter,
-          placeholder: "What needs to be done?",
-          autoFocus: true })
-      );
-    }
-  }]);
-
-  return HEADER;
-}(Component);
-
-var MAIN = function (_Component2) {
-  _inherits(MAIN, _Component2);
-
-  function MAIN() {
-    _classCallCheck(this, MAIN);
-
-    return _possibleConstructorReturn(this, (MAIN.__proto__ || Object.getPrototypeOf(MAIN)).apply(this, arguments));
-  }
-
-  _createClass(MAIN, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "section",
-        { className: "main" },
-        React.createElement("input", { className: "toggle-all", type: "checkbox" }),
-        React.createElement(
-          "label",
-          { htmlFor: "toggle-all" },
-          "Mark all as complete"
-        ),
-        React.createElement(LIST, { count: this.props.count, listItem: this.props.listItem })
-      );
-    }
-  }]);
-
-  return MAIN;
-}(Component);
-
-var TodoItem = function (_Component3) {
-  _inherits(TodoItem, _Component3);
-
-  function TodoItem() {
-    _classCallCheck(this, TodoItem);
-
-    return _possibleConstructorReturn(this, (TodoItem.__proto__ || Object.getPrototypeOf(TodoItem)).apply(this, arguments));
-  }
-
-  _createClass(TodoItem, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "li",
-        null,
-        this.props.text
-      );
-    }
-  }]);
-
-  return TodoItem;
-}(Component);
-
-var LIST = function (_Component4) {
-  _inherits(LIST, _Component4);
-
-  function LIST() {
-    _classCallCheck(this, LIST);
-
-    return _possibleConstructorReturn(this, (LIST.__proto__ || Object.getPrototypeOf(LIST)).apply(this, arguments));
-  }
-
-  _createClass(LIST, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "ul",
-        { className: "todo-list" },
-        this.props.todoList.map(function (i) {
-          return React.createElement(TodoItem, { listItem: i.listItem });
-        })
-      );
-    }
-  }]);
-
-  return LIST;
-}(Component);
-
-var CountDisplay = function (_Component5) {
-  _inherits(CountDisplay, _Component5);
-
-  function CountDisplay() {
-    _classCallCheck(this, CountDisplay);
-
-    return _possibleConstructorReturn(this, (CountDisplay.__proto__ || Object.getPrototypeOf(CountDisplay)).apply(this, arguments));
-  }
-
-  _createClass(CountDisplay, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "span",
-        { className: "todo-count" },
-        this.props.count
-      );
-    }
-  }]);
-
-  return CountDisplay;
-}(Component);
-
-var FOOTER = function (_Component6) {
-  _inherits(FOOTER, _Component6);
-
-  function FOOTER() {
-    _classCallCheck(this, FOOTER);
-
-    return _possibleConstructorReturn(this, (FOOTER.__proto__ || Object.getPrototypeOf(FOOTER)).apply(this, arguments));
-  }
-
-  _createClass(FOOTER, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "footer",
-        { className: "footer" },
-        React.createElement(CountDisplay, { count: this.props.count }),
-        React.createElement(
-          "button",
-          { className: "clear-completed" },
-          "Clear completed"
-        )
-      );
-    }
-  }]);
-
-  return FOOTER;
-}(Component);
+	return CountDisplay;
+}(React.Component);
 
 ReactDOM.render(React.createElement(TodoApp, null), document.getElementById('root'));
--- b03901156_hw4/src/index.js
+++ b03901156_hw4/src/index.js
@@ -1,117 +1,134 @@
 const { Component } = React;
 
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0, todoList: [] };
+class TodoApp extends Component {
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			todos: [
+			  {
+			    value: '',
+			    completed: false,
+			  },
+			],
+			text: '',		
+		};
+		this.updateValue = this.updateValue.bind(this);
+		this.handleEnter = this.handleEnter.bind(this);
+		this.Destroy = this.Destroy.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.DisplayLeft = this.DisplayLeft.bind(this);
+		this.DisplaytodoItem = this.DisplaytodoItem.bind(this);
+	}
 
-  // bind
-    this.createNewList = this.createNewList.bind(this);
-  }
+	updateValue(e){
+		this.setState({
+			text: e.target.value
+		});
+	}
 
-  createNewList(listItem) {
-    const newList = this.state.todoList.slice();
-    newList.push(listItem);
-    this.setState({
-      count: this.state.count + 1,
-      todoList: newList,
-    });
-  } // another way: this.setState({count: this.state.count+1;
-    // todoList: this.state.todoList.concat([{text: listItem}])})
-  render() {
-    return (
-      <section className="todoapp">
-        <HEADER createNewList={this.createNewList} / >
-        <MAIN count={this.state.count} todoList={this.state.todoList} / >
-        <FOOTER count={this.state.count} / >
-      </section>
-  );
-  }
+	handleEnter(ele){
+		if (ele.keyCode === 13 && this.state.text.trim() !== '') {
+			this.setState({
+				todos: [
+				  { value: this.state.text,
+					completed : false},
+					...todos,
+				],
+				text:'',
+			});
+			console.log("enter on");
+		};
+	}
+
+	Destroy(idx){
+		this.setState({
+			todos: this.state.todos.splice(idx, 1)
+		});
+	}
+
+	handleClick(idx){
+		var tmptodos = [...this.state.todos];
+		tmptodos[idx].completed = !tmptodos[idx].completed;
+		this.setState({ todos: tmptodos });
+	}
+
+	
+
+	DisplaytodoItem(lists, idx){
+		return(
+		<TodoItem 
+			onCheckClick={()=> this.handleClick(idx)}
+			onCancel={() => this.Destroy(idx)}
+			content={lists["value"]}
+			completed={lists["completed"]}
+			key={idx}
+			/>
+		);
+	}
+
+	render() {
+		var{todos, text} = this.state;
+		return (
+			 <section className="todoapp">
+				<header className="header">
+					<h1>todos</h1>
+				<input className="new-todo"
+					   value = {text}
+					   onChange = {this.updateValue}
+					   onKeyDown = {this.handleEnter} 
+						placeholder="What needs to be done?" 
+						autofocus/>
+				</header>
+				<ul class="todo-list">
+				  {todos.map((list,idx) => this.DisplaytodoItem(list,idx))}
+				</ul>
+				<CountDisplay />
+			</section>
+		);
+	}
 }
 
-HEADER.propTypes = {
-  createNewList: React.PropTypes.func.isRequired,
-};
+class TodoItem extends React.Component {
+	render(){
+		const {completed} =this.props;
+		return (
+			 <li className={completed? "done" : ""}>
+			   <input className="toggle" 
+			   		  type="checkbox"
+			   		  checked={completed}
+			   		  onChange={(e) => this.props.handleClick()}/>
+			   <label>{this.props.content}</label>
+			   <button className="delete"
+			   			onChange={(e)=>this.props.Destroy()}></button>
+			 </li>
+		);
+	}
+}	
 
-class HEADER extends Component {
-  constructor(props) {
-    super(props);
-    this.handleEnter = this.handleEnter.bind(this);
-  }
 
-  handleEnter(ele) {
-    if (eve.keyCode === 13 && ele.target.value !== '') {
-      this.props.createNewList(ele.target.value);
-    }
-  }
-  render() {
-    return (
-      <header className="header">
-        <h1>todos</h1>
-        <input
-          className="new-todo"
-          onKeyDown={this.handleEnter}
-          placeholder="What needs to be done?"
-          autoFocus / >
-      </header>
-  );
-  }
+class CountDisplay extends React.Component {
+	DisplayLeft(){
+  		var count;
+  		for(var i=0;i<this.props.todos.length;i+=1){
+    		if(this.props.todos[i].completed){
+      			count+=1;
+    		}
+  		}	
+  		if(count !== 1) {
+    		return <span>{count} items left</span>;
+  		}else if(count === 1) {
+    		return <span>1 item left</span>;
+  		}else{
+    		return <span>NO items</span>;
+  		}
+	}
+	render(){
+		return (
+      		<footer className="todo-count">
+        	{this.DisplayLeft()}
+      		</footer>
+    	);
+	};
 }
 
-
-class MAIN extends Component {
-  render() {
-    return (
-      <section className="main">
-        <input className="toggle-all" type="checkbox" / >
-        <label htmlFor="toggle-all">Mark all as complete</label>
-        <LIST count={this.props.count} listItem={this.props.listItem} / >
-      </section>
-  );
-  }
-}
-class TodoItem extends Component {
-  render() {
-    return <li>{this.props.text}</li>;
-  }
-}
-
-class LIST extends Component {
-  render() {
-    return (
-      <ul className="todo-list">
-        {
-  this.props.todoList.map(function (i) {
-    return (
-      <TodoItem listItem={i.listItem}/>
-  );
-  })
-  }
-      </ul>
-  );
-  }
-}
-class CountDisplay extends Component {
-  render() {
-    return (
-      <span className="todo-count">{this.props.count}</span>
-    );
-  }
-}
-class FOOTER extends Component {
-  render() {
-    return (
-      <footer className="footer">
-        <CountDisplay count={this.props.count} />
-        <button className="clear-completed">Clear completed</button>
-      </footer>
-  );
-  }
-}
-
-
-
-ReactDOM.render(
-  <TodoApp / >,
-  document.getElementById('root')
-);
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
